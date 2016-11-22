cc98c87f84793460c053fef3ff72efa66ae63e92
Heriberto Basulto <basulto93@live.com>
2016-11-22 15:41:59 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from 'react-dom';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new components. This component should 
+// produce some HTML
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// const = declaring a varible, final value.
+const App = function() {
+	return <div>Hi!</div>;
+}
+
+// Take this components generated HTML and put it 
+// on the page (in the DOM)
+
+
+// ****** FIX THIS, left off on video #11 *****
+ReactDom.render(<App/>);
